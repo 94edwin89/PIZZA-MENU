b1902dda4a98d667ae53d1955061aae86368b29f
Edwin Jude <edwin.judefrancis@gmail.com>
2024-04-22 21:37:58 +0530

    components & resuses

--- src/index.js
+++ src/index.js
@@ -2,16 +2,33 @@ import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 
 function App() {
-  return <h1>Hello React!</h1>;
+    return (
+        <div>
+            <Pizza/>
+            <Pizza />
+            <Pizza/>
+        </div>
+    )
+}
+
+function Pizza() {
+    return (
+        <div>
+            <img src="pizzas/spinaci.jpg" alt="pizza spinaci" />
+            <h2>Pizza Spinaci</h2>
+            <p>tomato, mozarella, spinach and ricotta cheese</p>
+        </div>
+    )
 }
 
 // React 18
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <StrictMode>
-    <App />
+    <StrictMode>
+      <App />
   </StrictMode>
 );
 
 // React Before 18
 // ReactDOM.render(<App/>,document.getElementById("root"));
+
