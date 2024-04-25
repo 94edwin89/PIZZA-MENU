c424c052cce64f6736d88b92250cf570c12b55fa
Edwin Jude <edwin.judefrancis@gmail.com>
2024-04-25 15:10:43 +0530

    React fragments & setting classes and text conditionally

--- src/index.js
+++ src/index.js
@@ -72,12 +72,19 @@ function Menu() {
   return (
     <main className="menu">
       <h2>Our Menu</h2>
+ 
       {numPizzas > 0 ? (
-        <ul className="pizzas">
-          {pizzaData.map((pizza) => (
-            <Pizza pizzaObj={pizza} key={pizza.name} />
-          ))}
-        </ul>
+        <>
+          <p>
+            Authentic Italian cuisine. 6 creative sihes to choose from. All from
+            our stone oven, all organic, all delicious.
+          </p>
+          <ul className="pizzas">
+            {pizzaData.map((pizza) => (
+              <Pizza pizzaObj={pizza} key={pizza.name} />
+            ))}
+          </ul>
+        </>
       ) : (
         <p>We're still working on our menu. Please come back later :)</p>
       )}
@@ -99,14 +106,15 @@ function Menu() {
   );
 }
 
-function Pizza({pizzaObj}) {
-  return (
-    <li className="pizza">
+function Pizza({ pizzaObj }) {
+    return (
+    //   ternary condition in classname 
+    <li className={`pizza ${pizzaObj.soldOut ? "sold-out" : ""}`}>
       <img src={pizzaObj.photoName} alt={pizzaObj.name} />
       <div>
         <h3>{pizzaObj.name}</h3>
         <p>{pizzaObj.ingredients}</p>
-        <span>{pizzaObj.price}</span>
+        <span>{pizzaObj.soldOut ? "SOLD OUT" : pizzaObj.price}</span>
       </div>
     </li>
   );
@@ -125,7 +133,7 @@ function Footer() {
   return (
     <footer className="footer">
       {isOpen ? (
-              <Order closeHour={closeHour} openHour={openHour} />
+        <Order closeHour={closeHour} openHour={openHour} />
       ) : (
         <p>
           We're happy to welcome you between {openHour}:00 and {closeHour}:00.
@@ -136,11 +144,12 @@ function Footer() {
   // return React.createElement('footer', null, "We're currently open!")
 }
 
-function Order({closeHour,openHour}) {
+function Order({ closeHour, openHour }) {
   return (
     <div className="order">
       <p>
-        We're open from {openHour}:00 to {closeHour}:00. Come visit us or order online
+        We're open from {openHour}:00 to {closeHour}:00. Come visit us or order
+        online
       </p>
       <button className="btn">Order</button>
     </div>
