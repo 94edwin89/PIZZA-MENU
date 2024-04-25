8dc44cd2ef4c69d57555ace2baccca2971cebacc
Edwin Jude <edwin.judefrancis@gmail.com>
2024-04-25 12:46:15 +0530

    extracting JSX into new component

--- src/index.js
+++ src/index.js
@@ -125,14 +125,24 @@ function Footer() {
   return (
     <footer className="footer">
       {isOpen ? (
-        <div className="order">
-          <p>We're open untill {closeHour}:00. Come visit us or order online</p>
-          <button className="btn">Order</button>
-        </div>
-          ) : <p>We're happy to welcome you between {openHour}:00 and {closeHour}:00.</p>}
+              <Order closeHour={closeHour } />
+      ) : (
+        <p>
+          We're happy to welcome you between {openHour}:00 and {closeHour}:00.
+        </p>
+      )}
     </footer>
   );
   // return React.createElement('footer', null, "We're currently open!")
+}
+
+function Order(props) {
+    return (
+        <div className="order">
+          <p>We're open untill {props.closeHour}:00. Come visit us or order online</p>
+          <button className="btn">Order</button>
+        </div>
+    )
 }
 
 // React 18
