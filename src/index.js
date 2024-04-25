e9569e683980e30eea63d12a9e4f4b9e5835f3ee
Edwin Jude <edwin.judefrancis@gmail.com>
2024-04-25 12:27:51 +0530

    rendering with && and terneries

--- src/index.js
+++ src/index.js
@@ -66,14 +66,22 @@ function Header() {
 }
 
 function Menu() {
+  const pizzas = pizzaData;
+  const numPizzas = pizzas.length;
+
   return (
     <main className="menu">
       <h2>Our Menu</h2>
-      <ul className="pizzas">
-        {pizzaData.map((pizza) => (
-          <Pizza pizzaObj={pizza} key={pizza.name} />
-        ))}
-      </ul>
+      {numPizzas > 0 ? (
+        <ul className="pizzas">
+          {pizzaData.map((pizza) => (
+            <Pizza pizzaObj={pizza} key={pizza.name} />
+          ))}
+        </ul>
+      ) : (
+        <p>We're still working on our menu. Please come back later :)</p>
+      )}
+
       {/* <Pizza
         name="Pizza Spinaci"
         ingredients="Tomato, mozarella, spinach, and ricotta cheese"
@@ -116,7 +124,12 @@ function Footer() {
 
   return (
     <footer className="footer">
-      {new Date().toLocaleTimeString()}.We're currently open!
+      {isOpen ? (
+        <div className="order">
+          <p>We're open untill {closeHour}:00. Come visit us or order online</p>
+          <button className="btn">Order</button>
+        </div>
+          ) : <p>We're happy to welcome you between {openHour}:00 and {closeHour}:00.</p>}
     </footer>
   );
   // return React.createElement('footer', null, "We're currently open!")
